a1724d30ac09223cb9ff7e454f4e15ddc18b110a
Brian Cook <bcrazorrider@gmail.com>
2020-08-16 19:39:50 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "Brian",
-  password: "Hefty131!",
-  database: "burgers_db"
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "Brian",
+    password: "Hefty131!",
+    database: "burgers_db",
+  });
+}
 
 
 connection.connect(function(err) {
