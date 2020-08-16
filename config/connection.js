aecb99cd6712166ec1615e2a9a7b1c263aae7d9b
Brian Cook <bcrazorrider@gmail.com>
2020-08-16 16:04:26 -0400

    fixing deploy problem

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,12 @@
-let mysql = require("mysql");
-let connection;
+var mysql = require("mysql");
 
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgersDB"
-  });
-}
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "Brian",
+  password: "Hefty131!",
+  database: "burgers_db"
+});
+
 
 connection.connect(function(err) {
   if (err) {
---
+++ public/assets/js/script.js
@@ -0,0 +1,30 @@
+$(function () {
+    console.log("Hello");
+    $(".burger-list").on("click", ".devouredbutton", function (event) {
+      console.log("devour");
+      event.preventDefault();
+      console.log($(this));
+      var id = $(this).data("id");
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: true,
+      }).then(function () {
+        console.log("devoured burgs");
+        location.reload();
+      });
+    });
+    $("#addButton").on("click", function (event) {
+      console.log("in the submit");
+      event.preventDefault();
+      var newBurgs = {
+        burger_name: $("#nb").val().trim(),
+      };
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurgs,
+      }).then(function () {
+        console.log("new burger created");
+        location.reload();
+      });
+    });
+  });
